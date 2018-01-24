37a41cfc8b2486c5f44f865c537644af650f366a
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-24 22:24:35 +0100

    Count commits

---
+++ collect_commits.py
@@ -0,0 +1,10 @@
+#!/bin/python
+
+# Creates a list with
+# [username], [commits]
+
+import json
+data = json.load(open('contributors'))
+
+for i in range(0, len(data)):
+  print(str(data[i]['author']['login']) + ',' + str(data[i]['total']))
